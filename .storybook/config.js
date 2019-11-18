87c42e6bb1521175da9a9abc4570d200f66d6566
Murillo Azambuja Goncalves <murilloagoncalves@gmail.com>
2019-11-18 10:11:10 -0300

    Docs and Tests

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
 import { GlobalStyle } from "../src/shared/global";
 
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
@@ -9,5 +11,11 @@ addDecorator(story => (
   </>
 ));
 
-// automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js|mdx
+configure(
+  [
+    require.context("../src", false, /Intro\.stories\.mdx/),
+    require.context("../src", true, /\.stories\.(js|mdx)$/)
+  ],
+  module
+);
